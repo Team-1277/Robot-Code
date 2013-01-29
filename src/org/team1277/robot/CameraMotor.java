0601112e8e34f1b2097c5d9099333059e518ae70
Nick B <>
2013-01-28 20:08:59 -0500

    Bunch of random stuff

--- src/org/team1277/robot/CameraMotor.java
+++ src/org/team1277/robot/CameraMotor.java
@@ -23,7 +23,7 @@ public class CameraMotor {
     
     public static void updateAngle() { 
         speed = MainRobot.leftStick.getThrottle() + 1d;
-        System.out.println(speed);
+        //System.out.println(speed);
         if(angleY > 0) {
             if(MainRobot.leftStick.getRawButton(MainRobot.BUTTON_CAMERA_SERVO_DOWN))
             {
