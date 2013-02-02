a1bb465829616948ac09ee50619ef278094607f0
Nick B <>
2013-02-02 13:17:37 -0500

    Basic encoder code

--- src/org/team1277/robot/ImageProcessor.java
+++ src/org/team1277/robot/ImageProcessor.java
@@ -18,16 +18,17 @@ public class ImageProcessor {
     
     
     
-    public static void Process(NetworkTable server) {
-        
+    public static void Process() {
+        NetworkTable server = NetworkTable.getTable("SmartDashboard");
         
         try
         {
+            System.out.println("\\/***\\/");
             //System.out.println(server.getNumber("test"));
-            if(server.containsKey("IMAGE_COUNT"))
-            {
-                System.out.println(server.getNumber("IMAGE_COUNT",0.0));
-            }
+            //Try updating roborealm
+            System.out.println(server.containsKey("IMAGE_COUNT"));
+            System.out.println(server.getNumber("IMAGE_COUNT",0.0));
+            System.out.println("/\\***/\\");
         }
         catch (TableKeyNotDefinedException ex)
         {
